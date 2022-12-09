cdf086b3bc1ff003baa9124b563d44e509592c7c
Ranjan Yadav <theranzanydv@gmail.com>
2022-12-09 19:41:42 +0545

    Centered Text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
 					<p className="text-4xl font-bold inline border-b-4 border-gray-500">
 						Contact
 					</p>
-					<p className="py-6">Get in touch</p>
+					<p className="py-6 text-center text-2xl">Get in touch</p>
 				</div>
 
 				<div className=" flex justify-center items-center">
@@ -22,19 +22,19 @@ const Contact = () => {
 							type="text"
 							name="name"
 							placeholder="Enter your name"
-							className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+							className="p-3 bg-transparent border-2 rounded-md text-white focus:outline-none"
 						/>
 						<input
 							type="email"
 							name="email"
 							placeholder="Enter your email"
-							className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+							className="my-4 p-3 bg-transparent border-2 rounded-md text-white focus:outline-none"
 						/>
 						<textarea
 							name="message"
 							placeholder="Enter your message"
 							rows="5"
-							className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+							className="p-3 bg-transparent border-2 rounded-md text-white focus:outline-none"
 						></textarea>
 
 						<button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
