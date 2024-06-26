36dee30b37f379dce05dfacfc13f5ef79009116e
Ranjan Yadav <theranzanydv@gmail.com>
2024-06-26 15:41:24 +0545

    refactor #2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
-import heroImage from '../assets/heroImage.png'
-import { MdKeyboardArrowRight } from 'react-icons/md'
-import { Link } from 'react-scroll'
+import heroImage from "../assets/heroImage.png";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
 	return (
 		<div
 			name="home"
-			className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+			className="min-h-screen h-full w-full bg-gradient-to-b from-black via-black to-gray-800"
 		>
 			<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
 				<div className="flex flex-col justify-center h-full">
